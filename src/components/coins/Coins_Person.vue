e9fc593980604d9a78fb89e28b30509f92ceb14b
严俊东 <1172482914@qq.com>
2017-03-09 20:39:23 +0800

    个人出售的金币和自定义金额

--- src/components/coins/Coins_Person.vue
+++ src/components/coins/Coins_Person.vue
@@ -1,5 +1,64 @@
 <template>
-  <div>个人金币单子</div>
+  <div class="coin-kc mt-20 bg-fff">
+    <ul class="clearfix">
+      <li class="border-bottom p-30">
+        <div class="clearfix">
+          <span class="fl f32 color-010101">20400万金</span>
+          <span class="fr f32 color-f75e46">500元</span>
+        </div>
+        <div class="clearfix mt-25">
+          <div class="fl f28 color-010101">
+            <span class="color-888">单价</span>1元=40.80万金
+          </div>
+          <div class="fr">
+            <i class="mr-20"><img src="/static/images/common/mobile.png"/></i><span class="f28 color-888">库存 1</span>
+          </div>
+        </div>
+      </li>
+      <li class="border-bottom p-30">
+        <div class="clearfix">
+          <span class="fl f32 color-010101">8012万金</span>
+          <span class="fr f32 color-f75e46">200元</span>
+        </div>
+        <div class="clearfix mt-25">
+          <div class="fl f28 color-010101">
+            <span class="color-888">单价</span>1元=40.80万金
+          </div>
+          <div class="fr">
+            <i class="mr-20"><img src="/static/images/common/mobile.png"/></i><span class="f28 color-888">库存 1</span>
+          </div>
+        </div>
+      </li>
+      <li class="border-bottom p-30">
+        <div class="clearfix">
+          <span class="fl f32 color-010101">20400万金</span>
+          <span class="fr f32 color-f75e46">500元</span>
+        </div>
+        <div class="clearfix mt-25">
+          <div class="fl f28 color-010101">
+            <span class="color-888">单价</span>1元=40.80万金
+          </div>
+          <div class="fr">
+            <i class="mr-20"><img src="/static/images/common/pc.png"/></i><span class="f28 color-888">库存 5</span>
+          </div>
+        </div>
+      </li>
+      <li class="border-bottom p-30">
+        <div class="clearfix">
+          <span class="fl f32 color-010101">3910万金</span>
+          <span class="fr f32 color-f75e46">200元</span>
+        </div>
+        <div class="clearfix mt-25">
+          <div class="fl f28 color-010101">
+            <span class="color-888">单价</span>1元=40.80万金
+          </div>
+          <div class="fr">
+            <i class="mr-20"><img src="/static/images/common/pc.png"/></i><span class="f28 color-888">库存 3</span>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
--- src/components/coins/Coins_Some.vue
+++ src/components/coins/Coins_Some.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>自定义金额购买</div>
+  <div>
+    <!---------------------------- 金币信息 ---------------------------->
+    <div class="mt-20 bg-f6 clearfix">
+      <ul class="tab-2 clearfix">
+        <li class="fl border-right text-center lh110 f28">
+          <span class="color-888 mr-20">单价</span>1元=40.2万金
+        </li>
+        <li class="fl text-center lh110 f28">
+          <span class="color-888 mr-20">库存</span>1,202,540万金
+        </li>
+      </ul>
+      <div class="bg-fff clearfix px-30">
+        <div class="fl lh110 w80 f32 color-000">购买数量<input class="coin-input lh110 pl-30" type="text" placeholder="不得少于单价数量" /></div>
+        <div class="fr lh110 f32 color-000">万金</div>
+      </div>
+    </div>
+    <!---------------------------- 购买 ---------------------------->
+    <div class="mt-20 bg-fff">
+      <div class="p-30">
+        <div class="f32 color-000">价格：<span class="color-f75e46">0元</span>（最少购买20元）</div>
+        <button class="w-100 bg-f75e46 bg-ccc lh110 color-fff f36 mt-30">立即购买</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
