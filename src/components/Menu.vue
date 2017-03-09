3391bab708e9bdb3b2cbe04175f3bad106687355
严俊东 <1172482914@qq.com>
2017-03-09 14:56:08 +0800

    调整基础结构

---
+++ src/components/Menu.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h1>辅助菜单</h1>
+    <ul>
+      <li>
+        <router-link
+          to="/list-coins?serverid=YXF16053120325978800016&areaid=YXQ16053120274791000015&gid=YX16053120241378200001&operatorId=undefined&operatorName=&gname=地下城与勇士&areaname=广东区&servername=广东1区&goodsType=3&typename=%2F%2F游戏币&gameImageUrl=">
+          广东一区
+        </router-link>
+      </li>
+      <li>
+        <router-link
+          to="/list-coins?serverid=YXF16053120330438300017&areaid=YXQ16053120274791000015&gid=YX16053120241378200001&operatorId=undefined&operatorName=&gname=地下城与勇士&areaname=广东区&servername=广东2区&goodsType=3&typename=%2F%2F游戏币&gameImageUrl=">
+          广东二区
+        </router-link>
+      </li>
+      <li>
+        <router-link
+          to="/list-coins?serverid=YXF16053120330995500018&areaid=YXQ16053120274791000015&gid=YX16053120241378200001&operatorId=undefined&operatorName=&gname=地下城与勇士&areaname=广东区&servername=广东3区&goodsType=3&typename=%2F%2F游戏币&gameImageUrl=">
+          广东三区
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default{}
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+    margin-top: 2em;
+    margin-bottom: 1em;
+  }
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  ul > li:nth-child(even) {
+    background-color: #bbefa0;
+  }
+
+  ul > li:nth-child(odd) {
+    background-color: #efbdcb;
+  }
+
+  li a {
+    border-bottom: 1px solid gainsboro;
+    padding: 20px 30px;
+    display: block;
+  }
+
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import ListCoins from '../components/ListCoins'
+import Menu from '../components/Menu'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path: '/list-coins',
+      name: 'ListCoins',
+      component: ListCoins
+    },
+    {
+      path: "/menu",
+      name: 'Menu',
+      component: Menu
+    },
+    {
+      path: "*",
+      redirect: "/menu"
     }
   ]
 })
